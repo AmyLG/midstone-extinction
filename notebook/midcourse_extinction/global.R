bf877ca1987737e06643dffa65391e1291808bb3
AmyLG <armelle.le.guelte@vanderbilt.edu>
2021-01-12 21:46:59 -0600

    shiny

--- notebook/midcourse_extinction/global.R
+++ notebook/midcourse_extinction/global.R
@@ -7,3 +7,5 @@ library(rgdal)
 
 ### Read the geospatial files using rgdal
 ecos_data <- rgdal::readOGR("../../data/ecos_us_ca_geo.geojson/")
+
+animal_group <- sort(unique(ecos_data$species_gr))
--- notebook/midcourse_extinction/server.R
+++ notebook/midcourse_extinction/server.R
@@ -1,8 +1,13 @@
 #
 # This is the server logic of a Shiny web application. 
 
-# Define server logic required to draw a histogram
+# Define server logic required to get the picture url
 shinyServer(function(input, output) {
-
-
+  src = "https://ecos.fws.gov/docs/species_images/doc4660.png"
+  output$picture <- renderText({c('<img src="',src,'"width="300" height="400">')})
+  
+# Define server logic required to get the picture url
+output$map <- renderLeaflet({leaflet(ecos_data[ecos_data$common_nam == "Akekee",]) %>%  addTiles() %>%
+  addPolygons(color = "purple", weight = 1, smoothFactor = 0.5, opacity = 1.0, fillOpacity = 0.2, fillColor = "purple")
+})
 })
